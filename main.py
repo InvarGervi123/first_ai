fe2347a7a547f6a9358d58090f38db481a02d68c
InvarGervi123 <68227462+InvarGervi123@users.noreply.github.com>
2024-07-05 21:59:43 +0300

    end and video part

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 import cv2
+import os
+
+#cap = cv2.VideoCapture("Tami_in_bus.mp4")
+
+files = os.listdir('images')
+
+for file in files:
+
+    img = cv2.imread('images/'+file)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    faces = cv2.CascadeClassifier('faces.xml')
+
+    results = faces.detectMultiScale(gray,scaleFactor=1.8,minNeighbors=4)
+
+    for (x,y,w,h) in results:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),thickness=3)
+
+
+
+        cv2.putText(img,"HOMO",(x+100,y-5),cv2.FONT_HERSHEY_PLAIN,8,(0,0,255),2)
+
+       # new_img = cv2.resize(img, (1920, 1080))
+
+    cv2.imshow('results',img)#new_img)
+    cv2.waitKey(0)
 
---
+++ main2.py
@@ -0,0 +1,5 @@
+import cv2
+import os
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+cap = cv2.VideoCapture('Tami_in_bus.mp4')
