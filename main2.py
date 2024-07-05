6867435f6a63dbd2c1b7395cce6be5216cf6ecd0
InvarGervi123 <68227462+InvarGervi123@users.noreply.github.com>
2024-07-05 23:05:39 +0300

    end and video part

--- main2.py
+++ main2.py
@@ -1,5 +1,49 @@
 import cv2
-import os
+import time
+
+
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-cap = cv2.VideoCapture('Tami_in_bus.mp4')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+cap = cv2.VideoCapture('images/Tami_in_bus.mp4')
+
+
+if (cap.isOpened() == False):
+    print("Error reading video file")
+
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+size = (frame_width, frame_height)
+
+result = cv2.VideoWriter('filename.mp4',
+                         cv2.VideoWriter_fourcc(*'MJPG'),
+                         10, size)
+
+
+while cap.isOpened():
+    _, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y , w ,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0 , 0), 3)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey ,ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0, 255, 0), 5)
+
+    # Display the output
+    ##cv2.imshow('img', img)
+
+    result.write(img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+result.release()
+
+
+cap.release()
